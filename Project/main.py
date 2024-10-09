44cde936b3ebc060dec7397a8149d1c20b3cc49d
Adilet Shildebayev <adilet.shildebayev@nu.edu.kz>
2024-10-09 19:34:27 +0500

    created dependency injection and implement basic request

--- Project/main.py
+++ Project/main.py
@@ -1,5 +1,27 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
 import models
-from database import engine
+from database import engine, SessionLocal
+
 app = FastAPI()
+
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@app.get("/")
+async def read_all(db: db_dependency):
+    return db.query(models.Todos).all()
