614cfc1a218492ccca9d6a92227cb6036910bffc
Adilet Shildebayev <adilet.shildebayev@nu.edu.kz>
2024-10-09 21:48:58 +0500

    Todo add to db method

--- Project/main.py
+++ Project/main.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
 from fastapi import FastAPI, Depends, HTTPException, Path
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from starlette import status
 
@@ -24,6 +25,13 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
+
 @app.get("/",  status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(models.Todos).all()
@@ -35,3 +43,11 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail='Todo not found')
+
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db:db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.dict())
+
+    db.add(todo_model)
+    db.commit()
