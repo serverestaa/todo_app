b3e20b60ef99e2770e22e8139458f18afc12d384
Adilet Shildebayev <adilet.shildebayev@nu.edu.kz>
2024-10-09 19:43:05 +0500

    search by id get request added

--- Project/main.py
+++ Project/main.py
@@ -1,8 +1,10 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
 from sqlalchemy.orm import Session
+from starlette import status
 
+from models import Todos
 import models
 from database import engine, SessionLocal
 
@@ -22,6 +24,14 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@app.get("/")
+@app.get("/",  status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(models.Todos).all()
+
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(models.Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail='Todo not found')
