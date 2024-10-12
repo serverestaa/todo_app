ae73e6475fa913be0c130ab04e2b5daa2faa6cae
Adilet Shildebayev <adilet.shildebayev@nu.edu.kz>
2024-10-12 18:39:57 +0500

    added users endpoints

---
+++ routers/users.py
@@ -0,0 +1,63 @@
+import os
+from typing import Annotated
+
+from dotenv import load_dotenv
+from fastapi import APIRouter, Depends, HTTPException, Path
+from passlib.context import CryptContext
+from pydantic import BaseModel, Field
+from sqlalchemy.orm import Session
+from starlette import status
+from models import Users
+from database import SessionLocal
+from routers.auth import get_current_user
+
+router = APIRouter(
+    prefix='/user',
+    tags=['user']
+)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+load_dotenv()
+bcrypt_schemes = os.getenv('BCRYPT_SCHEMES')
+
+SECRET_KEY = os.getenv('SECRET')
+bcrypt_context = CryptContext(schemes=[bcrypt_schemes], deprecated='auto')
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
+
+@router.get("/info", status_code=status.HTTP_200_OK)
+async def read_user_info(user: user_dependency,
+                         db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+
+    return db.query(Users).filter(Users.id == user.get('id')).first()
+
+
+class UserVerification(BaseModel):
+    password: str
+    new_password: str = Field(min_length=6)
+
+
+@router.put("/{password}", status_code=status.HTTP_204_NO_CONTENT)
+async def change_password(user: user_dependency,
+                          db: db_dependency,
+                          user_verification: UserVerification):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+
+    user_model = db.query(Users).filter(Users.id == user.get('id')).first()
+    if not bcrypt_context.verify(user_verification.password, user_model.hashed_password):
+        raise HTTPException(status_code=401, detail='Error on password change')
+    user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
+    db.add(user_model)
+    db.commit()
